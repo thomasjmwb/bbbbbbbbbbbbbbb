1273cad9a128cc6e56737f7c376cf7f4c3ecafa5
sirzach <zach.mcgonigal@gmail.com>
2014-09-15 22:23:08 -0400

    a not so good half-distracted attempt at getting login up and running

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login');
+  this.resource('decks', function () {
+    this.resource('deck', {path: '/:id'});
+  });
 });
 
 export default Router;
