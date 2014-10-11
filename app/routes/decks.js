56cdbf2510627cd5e09d62ef75c78d14019d2254
sirzach <zach.mcgonigal@gmail.com>
2014-10-11 15:22:34 -0400

    getting some temporary stuff in

--- app/routes/decks.js
+++ app/routes/decks.js
@@ -4,9 +4,8 @@ export default Ember.Route.extend({
   beforeModel: function () {
     var cardsController = this.controllerFor('cards');
     return Ember.$.get('/api/cards', function (data) {
-      console.log(Em.keys(data.cards[0]).length);
       var cards = [],
-          cardKeys = Em.keys(data.cards[0]).slice(0, 100);
+          cardKeys = Ember.keys(data.cards[0]);
       cardKeys.forEach(function (card) {
         cards.push(data.cards[0][card]);
       });
