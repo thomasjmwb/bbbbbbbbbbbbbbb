077d987a5dbb61dbfbcf65549d139a01226b6fc9
sirzach <zach.mcgonigal@gmail.com>
2014-10-17 23:21:21 -0400

    more code cleanup

--- app/models/deck.js
+++ app/models/deck.js
@@ -1,14 +1,34 @@
 import Ember from 'ember';
 
+function groupByName (prev, curr) {
+  var cardObj = prev.findBy('card.name', curr.name);
+
+  if (cardObj) {
+    cardObj.count++;
+  } else {
+    prev.push({
+      count: 1,
+      card: curr
+    });
+  }
+
+  return prev;
+}
+
 export default Ember.Object.extend({
+  /** @property {String} - the deck name */
   name: null,
 
+  /** @property {String} - user defined comments about the deck */
   comments: null,
 
+  /** @property {Array[Card]} - an array of Cards that make up the main deck */
   cards: [],
 
+  /** @property {Array[Card]} - an array of Cards that make up the sideboard */
   sideboard: [],
 
+  /** @property {Number} - number of instants in the main deck */
   numMainDeckInstants: function () {
     var mainDeckInstants = this.get('mainDeckInstants') || [];
 
@@ -17,25 +37,15 @@ export default Ember.Object.extend({
     }, 0);
   }.property('mainDeckInstants.@each'),
 
+  /** @property {Array[CardObj]} - an array of CardObjs consisting of:
+    * count - the number of cards of that name
+    * card - the actual Card
+    */
   mainDeckInstants: function () {
     var instants = this.get('cards').filter(function (card) {
       return card.types[0].toLowerCase() === 'instant';
     });
 
-    return instants.reduce(function (prev, curr) {
-      var cardObj = prev.findBy('name', curr.name);
-
-      if (cardObj) {
-        cardObj.count++;
-      } else {
-        prev.push({
-          name: curr.name,
-          count: 1,
-          card: curr
-        });
-      }
-
-      return prev;
-    }, []);
+    return instants.reduce(groupByName, []);
   }.property('cards.@each')
 });
