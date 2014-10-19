20f3909b693dfa3332ba61cc9205c94af54b968c
Zach McGonigal <zach.mcgonigal@gmail.com>
2014-10-18 20:42:34 -0400

    allow for more than four lands. duh

--- app/controllers/card.js
+++ app/controllers/card.js
@@ -1,13 +1,19 @@
 import Ember from 'ember';
 
+var basicLands = ['Plains', 'Island', 'Swamp', 'Mountain', 'Forest', 'Snow-Covered Plains',
+  'Snow-Covered Island', 'Snow-Covered Swamp', 'Snow-Covered Mountain', 'Snow-Covered Forest'];
 export default Ember.ObjectController.extend({
   needs: ['deck'],
 
   canAddToDeck: function () {
     var cardsInDeck = this.get('controllers.deck.cards'),
-        currentCard = this.get('model');
+        currentCardName = this.get('model.name');
 
-    return cardsInDeck.filterBy('name', currentCard.get('name')).length < 4;
+    if (basicLands.contains(currentCardName)) {
+      return true;
+    }
+
+    return cardsInDeck.filterBy('name', currentCardName).length < 4;
   }.property('controllers.deck.cards.@each'),
 
   cannotAddToDeck: Ember.computed.not('canAddToDeck')
