5cafef64cd3196d83de747104c9f200e1ee1bf37
Zach McGonigal <zach.mcgonigal@gmail.com>
2014-10-18 20:37:54 -0400

    disallowing a card to be added to the main deck more than 4 times

--- app/controllers/card.js
+++ app/controllers/card.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  needs: ['deck'],
 
+  canAddToDeck: function () {
+    var cardsInDeck = this.get('controllers.deck.cards'),
+        currentCard = this.get('model');
+
+    return cardsInDeck.filterBy('name', currentCard.get('name')).length < 4;
+  }.property('controllers.deck.cards.@each'),
+
+  cannotAddToDeck: Ember.computed.not('canAddToDeck')
 });
