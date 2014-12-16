f14f74131869df4d68a7fc7bc6bab2175ef08451
sirzach <zach.mcgonigal@gmail.com>
2014-12-15 19:50:34 -0500

    upgraded and removed ember-data. also added ember-inflector since that was included in ember-data

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-window.Em = Ember;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
