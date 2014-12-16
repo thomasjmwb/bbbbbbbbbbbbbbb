c368fb901b61d8f3a81a59d617b32346200fca04
sirzach <zach.mcgonigal@gmail.com>
2014-12-15 20:02:15 -0500

    ok, we also need ember-inflector from bower

--- app/helpers/pluralize-string.js
+++ app/helpers/pluralize-string.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
-export default Ember.Handlebars.makeBoundHelper(function(value, options) {
+export default Ember.Handlebars.makeBoundHelper(function(value) {
   return value.pluralize();
 });
