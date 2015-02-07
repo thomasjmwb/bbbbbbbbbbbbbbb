384a7b2eabaab2768c48777cb5220c647e9b832d
sirzach <zach.mcgonigal@gmail.com>
2015-02-07 15:29:38 -0500

    upgraded to ember 1.9.1

--- app/controllers/decks/import-deck.js
+++ app/controllers/decks/import-deck.js
@@ -2,17 +2,19 @@ import Ember from 'ember';
 import Deck from 'webatrice/models/deck';
 
 export default Ember.ObjectController.extend({
+  needs: ['cards'],
+
   actions: {
     close: function() {
       return this.send('closeModal');
     },
 
     importDeck: function () {
-      var cards = this.controllerFor('cards').get('model'),
+      var cards = this.get('controllers.cards.model'),
         deck = Deck.createDeck(this.get('importContents'), cards);
 
       this.send('closeModal');
-      this.transitionTo('deck.build', deck);
+      this.transitionToRoute('deck.build', deck);
     }
   }
 });
